4aef9773603977e0518cab03faf2d2d9ad2ff4c7
Suvanga <suvanga.dhz@gmail.com>
2024-10-13 09:39:41 -0400

    Restablishing roots with python for leet code preparation

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 
 #adding two numbers for the tutorial  time in python
 
-num1 = input("Enter your Number:")
-num2  = input("Enter your second number")
-print("Sum:", int(num1)+int(num2))
+num= input("Weight: ")
+weight = input("(K)g ir (L)bs")
+
+if weight =="K" or weight =='k':
+    print("Weight in Kg: ", int(num)//0.453)
+
+elif weight == "l":
+    print("Weight in (L)bs: ", num)
 
