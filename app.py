6ee6fa619c1eab36e17b82137a0107a9a1515b1d
Suvanga <suvanga.dhz@gmail.com>
2024-10-13 09:42:11 -0400

    Restablishing roots with python for leet code preparation

--- app.py
+++ app.py
@@ -2,11 +2,11 @@
 #adding two numbers for the tutorial  time in python
 
 num= input("Weight: ")
-weight = input("(K)g ir (L)bs")
+unit = input("(K)g ir (L)bs")
 
-if weight =="K" or weight =='k':
+if unit =="K" or weight =='k':
     print("Weight in Kg: ", int(num)//0.453)
 
-elif weight == "l":
+elif unit == "l":
     print("Weight in (L)bs: ", num)
 
