1a29f46ebd6f22ed5066c1744dcd9500ab95e29d
Suvanga <suvanga.dhz@gmail.com>
2024-10-13 09:55:36 -0400

    Restablishing roots with python for leet code preparation

---
+++ Loops.py
@@ -0,0 +1,10 @@
+
+# Flag of NepAL
+def my_function():
+    i = 1
+    while i<=10:
+        print(i * "*")
+        i+=1
+
+my_function()
+my_function()
--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 
 #adding two numbers for the tutorial  time in python
 
-num= input("Weight: ")
+num= int(input("Weight: "))
 unit = input("(K)g ir (L)bs")
 
-if unit =="K" or weight =='k':
+if unit =="K" or unit =='k':
     print("Weight in Kg: ", int(num)//0.453)
 
 elif unit == "l":
+    num = num*0.45
     print("Weight in (L)bs: ", num)
-
