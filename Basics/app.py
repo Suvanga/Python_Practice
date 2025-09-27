db73943cb5f857637980ed3130e2aaa11b91a92d
Suvanga <suvanga.dhz@gmail.com>
2025-09-27 19:45:29 -0400

    Basic readme and basic python processes

--- Basics/app.py
+++ Basics/app.py
@@ -1,12 +1,67 @@
+#Here is the reference review file which you can simply copy and paste to test:
+# age = 12
+#
+# print(age)
+#
+# name = input("What is your name? ")
+#
+# print("Hello " + name )
+#
+#
+# birth_year = input("what is your year? : ")
+#
+# age = 2025 - int(birth_year)
+#
+# print(age)
+#
+#
+# weight = int(input("Weight: "))
+# catagories = input("W (K)g or (L)bs: ")
+#
+# match catagories:
+#     case "K" | "k" :
+#         print("weight in Lbs " + str( weight*2.2))
+#     case "l" | "L":
+#         print("weight weight in Kgs " +str( weight/2.2))
 
-#adding two numbers for the tutorial  time in python
+# names =["John", "Bob", "mosh", "Suv", "Marry"]
+#
+# names[0] = "don"
+#
+# print(names[0:3])
+#
+# numbers =[1,2,3,4,5]
+#
+# numbers.insert(-2, 3)
+# numbers.remove(3)
+#
+# print(1 in numbers)
+#
+# print(len(numbers))
+#
+#
 
-num= int(input("Weight: "))
-unit = input("(K)g ir (L)bs")
+#
+# print(number)
+#
+# _____________________________
+number = [1,2,3,4,5]
+i= 0
+while i < len(number) :
+        print(number[i])
+        i=i+1
 
-if unit =="K" or unit =='k':
-    print("Weight in Kg: ", int(num)//0.453)
+print("instead of that we could simply use for item in number ")
+for item in number:
+    print(item)
+# ____________________________
+#now about tuples
+print("use of range reference")
 
-elif unit == "l":
-    num = num*0.45
-    print("Weight in (L)bs: ", num)
+for items in range(5,10,2): #jumps step by 2 and also the range is from 5 to 10
+    print(items)
+
+#tuples are inmutable and they are similar to list and if you want them to not change your list then you use tuple :)
+
+num = (1,2,3)
+num.count()
